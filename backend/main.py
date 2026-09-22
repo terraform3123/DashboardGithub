fdf8eb3b34d92633053152bb0aa70ec66a081c14
FernandoFlorentino77 <fernandoflorentino1905@gmail.com>
2026-09-21 22:30:18 -0300

    feat: Adiciona mais informações ao buscar usuário

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="GITHUB_DASHBOARD")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http//localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]

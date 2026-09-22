4889c27f1ed14d4705475940639a2cdb59bd774d
terraform3123 <ticodoidospotify@gmail.com>
2026-09-21 22:33:18 -0300

    feat: front recebendo dados

--- frontend/fetch.js
+++ frontend/fetch.js
@@ -1,27 +1,9 @@
 import axios from "axios";
 
-import axios from "axios";
+try {
+    const response = await axios.get("http://127.0.0.1:8000/usuario/Lucas0M");
 
-async function buscarUsuario() {
-    try {
-        const response = await axios.get(
-            "http://127.0.0.1:8000/usuario/LucasRRibeiro"
-        );
-
-        console.log("Resposta:", response.data);
-
-        const resultado = document.getElementById("resultado");
-
-        if (!resultado) {
-            console.error("Elemento #resultado não encontrado!");
-            return;
-        }
-
-        resultado.textContent = JSON.stringify(response.data, null, 2);
-
-    } catch (error) {
-        console.error("Erro na requisição:", error);
-    }
+    console.log(response.data);
+} catch (error) {
+    console.log(error);
 }
-
-buscarUsuario();

084bc1359cb14542883f6bb795323e5271ad3032
FernandoFlorentino77 <fernandoflorentino1905@gmail.com>
2026-09-21 22:09:36 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from usuario_routes import router as router_usuario
 
-app = FastAPI(title="DashboardGITHUB")
+app = FastAPI(title="GITIEADWDS")
 
 app.include_router(router_usuario)

304cb2606ea36a0131c8bb2a2b06703d415aaa49
FernandoFlorentino77 <fernandoflorentino1905@gmail.com>
2026-09-21 22:13:23 -0300

    bug fix: Arruma o CORS

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from usuario_routes import router as router_usuario
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="GITIEADWDS")
+app = FastAPI(title="GITHUB_DASHBOARD")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http//localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(router_usuario)

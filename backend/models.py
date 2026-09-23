2ca8f59f524f4fcfdc8fb9ee03c5eaeb72080fa5
Fernando <fernandoflorentino1905@gmail.com>
2026-09-23 13:28:39 -0300

    fix: Corrige erro string_type do modelo Dados

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Dados(BaseModel):
     login: str
-    nome: str
-    qtd_repositorios_publicos: int
-    seguidores: int
-    seguindo: int
-    bio_user: str
+    nome: Optional[str] = None
+    qtd_repositorios_publicos: int = 0
+    seguidores: int = 0
+    seguindo: int = 0
+    bio_user: Optional[str] = None
     avatar_user: str
--- backend/usuario_routes.py
+++ backend/usuario_routes.py
@@ -5,18 +5,18 @@ from models import Dados
 
 router = APIRouter()
 
-@router.get("/usuario/{usuario_id}",response_model=Dados)
+@router.get("/usuario/{usuario_id}", response_model=Dados)
 def buscar_usuario(usuario_id: str):
     try:
         user = git_api.get_user(usuario_id)
-        return Dados(
-            login=user.login,
-            nome=user.name,
-            qtd_repositorios_publicos=user.public_repos,
-            seguidores=user.followers,
-            seguindo=user.following,
-            bio_user=user.bio,
-            avatar_user=user.avatar_url
-        )
+        return {
+            "login":user.login,
+            "nome":user.name,
+            "qtd_repositorios_publicos":user.public_repos,
+            "seguidores":user.followers,
+            "seguindo":user.following,
+            "bio_user":user.bio,
+            "avatar_user":user.avatar_url
+        }
     except error_not_user:
         return {"erro": "Usuário não encontrado"}

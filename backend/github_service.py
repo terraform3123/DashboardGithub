9997c5b161483fdb733c920c93a724eacbffad14
FernandoFlorentino77 <fernandoflorentino1905@gmail.com>
2026-09-22 22:02:01 -0300

    feat: Adiciona o modelo Dados

--- backend/github_service.py
+++ backend/github_service.py
@@ -1,5 +1,7 @@
 import os
 from github import Github, UnknownObjectException
+from dotenv import load_dotenv
 
+load_dotenv()
 git_api = Github(os.environ.get("GITHUB_TOKEN"))
 error_not_user = UnknownObjectException
--- backend/usuario_routes.py
+++ backend/usuario_routes.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter
 from github_service import git_api, error_not_user
+from models import Dados
+
 
 router = APIRouter()
 
-@router.get("/usuario/{usuario_id}")
+@router.get("/usuario/{usuario_id}",response_model=Dados)
 def buscar_usuario(usuario_id: str):
     try:
-        usuario = git_api.get_user(usuario_id)
-        return {
-            "login": usuario.login,
-            "nome": usuario.name,
-            "qtd_repositorios_publicos": usuario.public_repos,
-            "seguidores": usuario.followers,
-            "seguindo": usuario.following,
-            "bio_user": usuario.bio,
-            "avatar_user": usuario.avatar_url
-        }
+        user = git_api.get_user(usuario_id)
+        return Dados(
+            login=user.login,
+            nome=user.name,
+            qtd_repositorios_publicos=user.public_repos,
+            seguidores=user.followers,
+            seguindo=user.following,
+            bio_user=user.bio,
+            avatar_user=user.avatar_url
+        )
     except error_not_user:
         return {"erro": "Usuário não encontrado"}

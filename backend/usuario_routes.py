582c3826539c8b291be045c00a831493ecf782f5
terraform3123 <you@example.com>
2026-09-24 21:01:21 -0300

    feat: stylezation frontend

--- backend/usuario_routes.py
+++ backend/usuario_routes.py
@@ -20,4 +20,4 @@ def buscar_usuario(usuario_id: str):
             "avatar_user":user.avatar_url
         }
     except error_not_user:
-        return JSONResponse(status_code=400, content={"erro": "Usuário não encontrado"})
+        return JSONResponse(status_code=404, content={"erro": "Usuário não encontrado"})
